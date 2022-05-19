499def170d2a33c8dcaf68afba570fa56897819d
윤형빈 <wudqls98@gmail.com>
2022-05-19 22:52:44 +0900

    윤형빈 : 수학

--- GameEngineBase/GameEngineTransform.h
+++ GameEngineBase/GameEngineTransform.h
@@ -15,9 +15,42 @@ public:
 	GameEngineTransform& operator=(const GameEngineTransform& _Other) = delete;
 	GameEngineTransform& operator=(GameEngineTransform&& _Other) noexcept = delete;
 
-protected:
+	// 로컬과 월드의 차이가 뭐냐
 
-private:
+	// 1의 크기를 가졌다.
 
+	// 부피 크기
+	float4 Scale;
+	// 회전 360분법
+	float4 Rotation; // Euler;
+	// 위치
+	float4 Position;
+
+public:
+	inline void SetScale(const float4& _Value)
+	{
+		Scale = _Value;
+	}
+	inline void SetRotation(const float4& _Value)
+	{
+		Rotation = _Value;
+	}
+	inline void SetPosition(const float4& _Value)
+	{
+		Position = _Value;
+	}
+
+	inline float4 GetScale()
+	{
+		return Scale;
+	}
+	inline float4 GetRotation()
+	{
+		return Rotation;
+	}
+	inline float4 GetPosition()
+	{
+		return Position;
+	}
 };
 
