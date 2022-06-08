ce0a7f7d8d143c5b7d11f0c588ce1836eb3db603
윤형빈 <wudqls98@gmail.com>
2022-06-08 20:10:30 +0900

    윤형빈 콘솔창

--- GameEngineContents/ContentsCore.cpp
+++ GameEngineContents/ContentsCore.cpp
@@ -4,23 +4,24 @@
 
 #pragma comment(lib, "GameEngineBase.lib")
 
-ContentsCore::ContentsCore() 
+ContentsCore::ContentsCore()
 	: GameEngineCore()
 {
 }
 
-ContentsCore::~ContentsCore() 
+ContentsCore::~ContentsCore()
 {
 }
 
 void ContentsCore::Start()
 {
+	GameEngineDebug::ConsoleOpen();
+
 	// 리소스를 로드하는데.
 
 	// RTTI 런 타임 타입 인포메이션
 	CreateLevel<TitleLevel>("Title");
 	CreateLevel<PlayLevel>("Play");
-
 	ChangeLevel("Play");
 
 	// 게임컨텐츠 정의
@@ -33,8 +34,8 @@ void ContentsCore::Update(float _DeltaTime)
 {
 	// 서버가 종료되었어.
 	// 무조건 경고 메세지창을 띄워줘야한다.
-	
-	
+
+
 }
 
 void ContentsCore::End()
