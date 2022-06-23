776b87fd551170b3d2ff0d93836d91362759cc8b
윤형빈 <wudqls98@gmail.com>
2022-06-23 16:46:08 +0900

    윤형빈 : 업데이트

--- GameEngineCore/GameEngineVertexBuffer.h
+++ GameEngineCore/GameEngineVertexBuffer.h
@@ -12,12 +12,9 @@ class GameEngineVertexBuffer : public GameEngineRes<GameEngineVertexBuffer>
 public:
 	friend GameEngineRes<GameEngineVertexBuffer>;
 
-private:
-	static GameEngineVertexBuffer* CreateRes(const std::vector<float4>& _Vertex, const std::string& _Name = "");
-
 public:
-	static void Create(const std::string& _Name, const std::vector<float4>& _Vertex);
-	static void Create(const std::vector<float4>& _Vertex);
+	static GameEngineVertexBuffer* Create(const std::string& _Name, const std::vector<float4>& _Vertex);
+	static GameEngineVertexBuffer* Create(const std::vector<float4>& _Vertex);
 
 private:
 	// constrcuter destructer
